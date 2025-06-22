9d76321944b5c51af19abdcf48ef3ddac7b59f33
Fred Snyder <fsnyder@castwide.com>
2025-06-21 20:42:15 -0400

    Logging

--- faceghost.js
+++ faceghost.js
@@ -2,11 +2,13 @@ function faceGhost() {
   document.querySelectorAll('[data-ad-rendering-role=profile_name]').forEach((elem) => {
     if (elem.innerHTML.includes('>Follow<') || elem.innerHTML.includes('>Join<')) {
       const parent = elem.closest('[data-virtualized=false]');
+      console.log('Faceghost sighting:', elem);
       if (parent) {
-        console.log('Ghost sighting:', elem);
-        console.log('Ghost container:', parent);
+        console.log('Faceghost container:', parent);
         parent.style.border = '1px solid #cc9999';
         parent.innerHTML = `<div style="text-align: center; color: #cc9999;">Ghosted</div>`;
+      } else {
+        console.error('Faceghost container not found');
       }
     }
   });
