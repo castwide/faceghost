65aa654540f1754fa3ad7e6bba0d30f5e20252dd
Fred Snyder <fsnyder@castwide.com>
2025-06-28 06:04:01 -0400

    DRY list processing

--- faceghost.js
+++ faceghost.js
@@ -9,15 +9,7 @@ function ghostableProfiles() {
   const list = [];
   document.querySelectorAll('[role=main] [data-ad-rendering-role=profile_name]').forEach((elem) => {
     if (elem.innerHTML.includes('>Follow<') || elem.innerHTML.includes('>Join<')) {
-      const header = elem.innerText.replace(/\s+/g, ' ').trim();
-      console.log('Faceghost sighting:', `"${header}"`, elem);
-      const parent = elem.closest('[data-virtualized=false]');
-      if (parent) {
-        console.log('Faceghost container:', parent);
-        list.push(elem.closest('[data-virtualized=false]'));
-      } else {
-        console.error('Faceghost container not found');
-      }
+      pushGhostable(elem, list);
     }
   });
   return list;
@@ -27,18 +19,22 @@ function ghostableReels() {
   const list = [];
   document.querySelectorAll('[role=main] .html-div').forEach((elem) => {
     if (elem.innerHTML.includes('>Reels<')) {
-      const header = elem.innerText.replace(/\s+/g, ' ').trim();
-      console.log('Faceghost sighting:', `"${header}"`, elem);
-      const parent = elem.closest('[data-virtualized=false]');
-      if (parent) {
-        console.log('Faceghost container:', parent);
-        list.push(elem.closest('[data-virtualized=false]'));
-      } else {
-        console.error('Faceghost container not found');
-      }
+      pushGhostable(elem, list);
     }
   });
   return list;
+}
+
+function pushGhostable(elem, list) {
+    const header = elem.innerText.replace(/\s+/g, ' ').trim();
+    console.log('Faceghost sighting:', `"${header}"`, elem);
+    const parent = elem.closest('[data-virtualized=false]');
+    if (parent) {
+      console.log('Faceghost container:', parent);
+      list.push(elem.closest('[data-virtualized=false]'));
+    } else {
+      console.error('Faceghost container not found');
+    }
 }
 
 faceGhost();
